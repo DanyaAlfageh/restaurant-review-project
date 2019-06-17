29ecfb86c642e3dd75dceaf209b2870a775f12f4
danyahalfageh <dalfakih@yahoo.com>
2019-06-17 14:39:19 +0300

    Added image and Review Validations

--- restaurant/app/models/image.rb
+++ restaurant/app/models/image.rb
@@ -5,7 +5,22 @@ class Image < ApplicationRecord
 
     validate :image_presence
 
-    def image_presence
-      errors.add(:image, "can't be blank") unless image.attached?
+    validate :image_type
+
+  private
+
+
+  def image_presence
+    errors.add(:image, "can't be blank") unless image.attached?
+  end
+
+  def image_type
+    if image.attached? == false
+      errors.add(:image, "is missing!")
     end
+    if !image.content_type.in?(%('image/jpeg image/png'))
+      errors.add(:image, "needs to be a jpeg or png!")
+    end
+  end
+  
 end
